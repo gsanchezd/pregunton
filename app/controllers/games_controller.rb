6eb2a7d803cbd187e23d3fdcd956f52d18fccb3a
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2014-08-18 04:31:52 -0400

    saving score in session instead of model for guest users

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,29 @@
 class GamesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :only => [:save]
 
   def play
 	@q_id = params[:q]
 	@alt_id = params[:alt]
-	@game = current_user.games.last
+	#@game = current_user.games.last
 	if @q_id.nil?
 		@question = Question.offset(rand(Question.count)).first
-		@game.increment_score
+		session[:score] += 1
 	else
 		@question = Question.find(@q_id)
 		@alternative = Alternative.find(@alt_id) 
 	end
+	@score = session[:score]
 
   end
 
   def new
-  	game = current_user.games.build(:score => -1)
-  	game.save
+	session[:score] = -1
+  	#game = current_user.games.build(:score => -1)
+  	#game.save
   	redirect_to games_play_path
   end
 
+  def save
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818062736) do
+ActiveRecord::Schema.define(version: 20140818080930) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20140818062736) do
     t.string   "provider"
     t.integer  "uid"
     t.string   "name"
+    t.boolean  "guest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
