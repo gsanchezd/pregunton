997c8287876b52b3921f050c2cabd4988b1a9f1a
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2014-08-27 02:05:59 -0400

    customizing index for question in activeadmin panel

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register Question do
   permit_params :question, :question_image, :questionnaire
 
+  index do
+    column :id
+    column :question
+    column :questionnaire
+    column :created_at
+    column :updated_at
+    actions
+  end
+
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
